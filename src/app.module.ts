b13ae97c47471cd0663a0643af93e9bad2529ab9
Natapong-rod <s6501012630069@email.kmutnb.ac.th>
2026-03-04 03:01:31 +0700

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -10,7 +11,27 @@ import { GuestUsersModule } from './guest_users/guest_users.module';
 import { CalibrationsModule } from './calibrations/calibrations.module';
 
 @Module({
-  imports: [DatabaseModule,EdgesModule,LandmarksModule, NodeModule, FloorModule, GuestUsersModule, CalibrationsModule],
+  
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'aws-1-ap-southeast-2.pooler.supabase.com',
+      port: 5432,
+      username: 'postgres.nskjeegyfgvaktuextiv',
+      password: 'xxxx',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    DatabaseModule,
+    EdgesModule,
+    LandmarksModule, 
+    NodeModule, 
+    FloorModule, 
+    GuestUsersModule, 
+    CalibrationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
