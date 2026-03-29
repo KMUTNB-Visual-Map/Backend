5c556653cd48987a886d819e951a410b637ad57b
Chinnabanchorn Yuenman <s6501012630051@email.kmutnb.ac.th>
2026-03-30 00:22:17 +0700

    last_update final for this year don't update again ...

--- src/get_pathfinding/dto/create-get_pathfinding.dto.ts
+++ src/get_pathfinding/dto/create-get_pathfinding.dto.ts
@@ -1,1 +1,10 @@
-export class CreateGetPathfindingDto {}
+export class CurrentLocationDto {
+  x: number;
+  y: number;
+  floor_id: number;
+}
+
+export class CreateGetPathfindingDto {
+  current: CurrentLocationDto;
+  destination_id: number;
+}
--- src/get_pathfinding/get_pathfinding.controller.ts
+++ src/get_pathfinding/get_pathfinding.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query} from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { GetPathfindingService } from './get_pathfinding.service';
+import { CreateGetPathfindingDto } from './dto/create-get_pathfinding.dto';
 
 @Controller('get-pathfinding')
 export class GetPathfindingController {
@@ -12,7 +13,27 @@ export class GetPathfindingController {
     @Query('floor_id') floor_id: string,
     @Query('destination') destination: string,
   ) {
-    const result = await this.getPathfindingService.path_finding(Number(x), Number(y),Number(floor_id), Number(destination),);
+    const result = await this.getPathfindingService.path_finding(
+      Number(x),
+      Number(y),
+      Number(floor_id),
+      Number(destination),
+    );
+    if (!result) {
+      return { message: 'No path found' };
+    }
+    return result;
+  }
+
+  @Post('get_path_finding')
+  async post_node(@Body() body: CreateGetPathfindingDto) {
+    const { current, destination_id } = body as any;
+    const result = await this.getPathfindingService.path_finding(
+      Number(current.x),
+      Number(current.y),
+      Number(current.floor_id),
+      Number(destination_id),
+    );
     if (!result) {
       return { message: 'No path found' };
     }
